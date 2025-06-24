31dcf02428861380e635a4daaa793e7bfe3d5c3e
Gabriel Teixeira Charles da Silva <63520287+gabrielTeixeira0021@users.noreply.github.com>
2025-06-24 09:50:37 -0300

    Update consulta_vagas_camas.sql

--- Albergue_SrAlmeida/consulta_vagas_camas.sql
+++ Albergue_SrAlmeida/consulta_vagas_camas.sql
@@ -1,15 +1,11 @@
--- ============================
--- ITEM 5: Mostrar Vagas Disponíveis e Camas Reservadas em um Determinado Dia
--- ============================
-
--- VAGAS RESERVADAS EM UM DIA ESPECÍFICO
+-- vagas reservadas em um dia específico 
 SELECT v.id_vaga, v.numero_cama, q.id_quarto
 FROM Vaga v
 JOIN Reserva_Vaga rv ON v.id_vaga = rv.id_vaga
 JOIN Quarto q ON v.id_quarto = q.id_quarto
 WHERE '2025-07-01' BETWEEN rv.data_entrada AND rv.data_saida;
 
--- VAGAS DISPONÍVEIS EM UM DIA ESPECÍFICO
+-- vagas disponíveis em um dia específico 
 SELECT v.id_vaga, v.numero_cama, q.id_quarto
 FROM Vaga v
 JOIN Quarto q ON v.id_quarto = q.id_quarto
